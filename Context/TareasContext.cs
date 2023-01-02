604e79cd31ea7d8912b76598f4b0a495784ee48e
Hector-Lopez <hectorrrr.lopezz@gmail.com>
2023-01-01 23:06:27 -0500

    Creando modelo de categoria con Fluent API

--- Context/TareasContext.cs
+++ Context/TareasContext.cs
@@ -8,4 +8,15 @@ public class TareasContext: DbContext
     public DbSet<Categoria> Categorias {get;set;}
     public DbSet<Tarea> Tareas {get;set;}
     public TareasContext(DbContextOptions<TareasContext> options) :base(options){ }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(categoria =>
+        {
+            categoria.ToTable("Categoria");
+            categoria.HasKey(p=>p.CategoriaId);
+
+            categoria.Property(p=>p.Nombre).IsRequired().IsRequired();
+            categoria.Property(p=> p.Descripcion);
+        });
+    }
 }
